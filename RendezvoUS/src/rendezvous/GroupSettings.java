a1439407d26516650743c30d52a2311257dbbc72
Noah Garcia <ndgarcia3882@gmail.com>
2023-11-27 21:11:03 -0600

    Tried implementing Group Tab

--- RendezvoUS/src/rendezvous/GroupSettings.java
+++ RendezvoUS/src/rendezvous/GroupSettings.java
@@ -2,62 +2,93 @@ package rendezvous;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
-//todo Class not used
+
 public class GroupSettings extends JPanel {
-    private MainScreen mainScreen;
+    private UserAccount userAccount;
+    private String username = null;
+    private GroupViewScreen groupCalendarPanel;
+    private final JTextArea groupList;
+    public GroupSettings(UserAccount userAccount, GroupViewScreen groupCalendarPanel) {
+        this.userAccount = userAccount;
+        this.groupCalendarPanel = groupCalendarPanel;
+        groupList = new JTextArea(20, 20);
 
-    public GroupSettings(MainScreen mainScreen) {
-        this.mainScreen = mainScreen;
-        setLayout(new BorderLayout());
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        add(createAppearanceSection(), BorderLayout.NORTH);
-        add(createAccountManagementSection(), BorderLayout.CENTER);
+        add(createGroupManagementSection());
     }
+    //List of Group members, Group Owner, Add/remove group Member
 
-    private JPanel createAppearanceSection() {
-        JPanel appearancePanel = new JPanel();
-        appearancePanel.setLayout(new FlowLayout());
-        appearancePanel.add(new JLabel("Appearance:"));
+    private JPanel createGroupManagementSection() {
+        JPanel groupAccountsPanel = new JPanel();
+        groupAccountsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        groupAccountsPanel.setBorder(BorderFactory.createTitledBorder("Group Members"));
 
-        JComboBox<ThemeManager.Theme> themeComboBox = new JComboBox<>(ThemeManager.Theme.values());
-        themeComboBox.addActionListener(e -> {
-            ThemeManager.Theme selectedTheme = (ThemeManager.Theme) themeComboBox.getSelectedItem();
-            mainScreen.changeTheme(selectedTheme); // Call changeTheme on MainScreen
+        groupList.setBorder(BorderFactory.createTitledBorder("Accounts in Group: "));
+        updateGroupList();
+
+        JButton addAccountButton = new JButton("Add Account");
+        addAccountButton.addActionListener(e -> {
+            username = promptForUsername();
+            addAccount(username);
         });
-        appearancePanel.add(themeComboBox);
 
-        return appearancePanel;
+        JButton removeAccountButton = new JButton("Remove Account");
+        removeAccountButton.addActionListener(e -> {
+            username = promptForUsername();
+            removeAccount(username);
+        });
+
+
+        groupAccountsPanel.add(groupList);
+        groupAccountsPanel.add(addAccountButton);
+        groupAccountsPanel.add(removeAccountButton);
+        //groupAccountsPanel.add(sendFeedbackButton);
+
+        return groupAccountsPanel;
     }
 
-    private JPanel createAccountManagementSection() {
-        JPanel accountPanel = new JPanel();
-        accountPanel.setLayout(new FlowLayout());
-
-        JButton deleteAccountButton = new JButton("Delete Account");
-        deleteAccountButton.addActionListener(e -> deleteAccount());
-        accountPanel.add(deleteAccountButton);
-
-        JButton sendFeedbackButton = new JButton("Send Feedback");
-        sendFeedbackButton.setEnabled(false); // Disabled as functionality not implemented yet
-        accountPanel.add(sendFeedbackButton);
-
-        return accountPanel;
-    }
-
-    private void deleteAccount() {
-        // Placeholder for delete account functionality
-        String username = getCurrentUser();
-        File file = new File(username + ".dat");
-        if (file.delete()) {
-            JOptionPane.showMessageDialog(this, "Account deleted successfully.");
-            System.exit(0); // Exit the application
-        } else {
-            JOptionPane.showMessageDialog(this, "Error deleting account.", "Error", JOptionPane.ERROR_MESSAGE);
+    private void updateGroupList() {
+        groupList.setText("");
+        for(int i = 0; i < groupCalendarPanel.groupCalendar.getAccountsInGroupSize(); i++){
+            groupList.append(groupCalendarPanel.groupCalendar.getAccountInGroup(i).getUsername() + "\n");
         }
     }
 
-    private String getCurrentUser() {
-        // Placeholder method for getting current user's username
-        return "current_user";
+    private String promptForUsername() {
+        String newUsername = null;
+        newUsername = JOptionPane.showInputDialog("Enter a Username:");
+        return newUsername;
     }
+
+    private void addAccount(String username) {
+        File file = new File(username + ".dat");
+        if (file.exists()) {
+            groupCalendarPanel.groupCalendar.addAccountToGroup(username);
+            updateGroupList();
+            JOptionPane.showMessageDialog(this, "Account added to Group.");
+        } else {
+            JOptionPane.showMessageDialog(this, "Error: account not found.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    private void removeAccount(String groupMember) {
+        boolean inGroup = false;
+        for(int i = 0; i < groupCalendarPanel.groupCalendar.getAccountsInGroupSize(); i++){
+            if(groupMember.equals(groupCalendarPanel.groupCalendar.getAccountInGroup(i).getUsername())){
+                inGroup = true;
+            }
+        }
+        if (groupMember.equals(userAccount.getUsername())){
+            JOptionPane.showMessageDialog(this, "Cannot remove group creator from group");
+        }
+        else if (inGroup) {
+            groupCalendarPanel.groupCalendar.removeAccountInGroup(groupMember);
+            updateGroupList();
+            JOptionPane.showMessageDialog(this, "Account removed from Group.");
+        } else {
+            JOptionPane.showMessageDialog(this, "Error: account not found.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+
 }
